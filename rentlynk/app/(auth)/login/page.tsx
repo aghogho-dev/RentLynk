0d6741fa589a24ecbe9a8f756e4c48d9575eb6ba
doctorzik <ezekieljeremiah92@gmail.com>
2025-07-23 23:03:15 +0100

    feat: implement login functionality with GitHub sign-in and session management

--- rentlynk/app/(auth)/login/page.tsx
+++ rentlynk/app/(auth)/login/page.tsx
@@ -1,59 +1,56 @@
-// rentlynk/app/(auth)/login/page.tsx
+
+
+
+
+import GithubSignIn from "@/components/molecules/GithubSignIn";
+import { Button } from "@/components/ui/button";
+import { signIn, signOut } from "@/lib/auth";
+import {redirect } from "next/navigation";
+
 export default function Login() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center px-6 min-h-screen bg-gray-100">
-      <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg">
-        <h2 className="text-3xl font-semibold text-gray-800 mb-6 text-center">Welcome Back</h2>
-        <form className="space-y-5">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Email address
-            </label>
-            <input
-              type="email"
-              id="email"
-              autoComplete="email"
-              required
-              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
+    <div>
+      Login Page
 
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              required
-              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
+      <GithubSignIn />
 
-          <div className="flex justify-between items-center">
-            <div className="text-sm">
-              <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
-                Forgot your password?
-              </a>
-            </div>
-          </div>
+      <form action={async (formdata) => {
+        "use server"
 
-          <button
-            type="submit"
-            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-xl shadow-sm transition duration-300"
-          >
-            Login in
-          </button>
-        </form>
+        try {
+          const formDataObj = Object.fromEntries(formdata.entries());
+          const response = await signIn("credentials", {
+            ...formDataObj,
+            redirect: false,
+            callbackUrl: "/",
+          })
+          if (response?.ok) {
+            redirect("/")
+          }
 
-        <p className="mt-6 text-sm text-center text-gray-600">
-          Don’t have an account?{" "}
-          <a href="/register" className="font-medium text-blue-600 hover:text-blue-500">
-            Sign up
-          </a>
-        </p>
-      </div>
+        } catch (error) {
+          console.log(error, "I am here")
+        }
+
+
+
+      }}>
+        <input type="email" name="email"></input>
+        <input name="password"></input>
+
+        <Button type="submit">Login</Button>
+      </form>
+
+
+
+      <form action={async () => {
+        "use server"
+        await signOut({ redirectTo: "/" })
+      }}>
+        <Button type="submit">
+          Sign Out
+        </Button>
+      </form>
     </div>
   );
 }
